94f10bb0111c4517eed71e0c320e167d49569748
lvminh97 <lvminh97@gmail.com>
2022-02-11 17:10:24 +0700

    Add sign up and login function (using Firebase)
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

---
+++ app/src/main/java/com/example/learn_english/Activity/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.example.learn_english.Activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.learn_english.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText emailEd, passwordEd;
+    private Button loginBtn, signupBtn;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initUI();
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initUI(){
+        loginBtn = findViewById(R.id.btn_login);
+        signupBtn = findViewById(R.id.btn_signup);
+        emailEd = findViewById(R.id.ed_email);
+        passwordEd = findViewById(R.id.ed_password);
+
+        SharedPreferences pref = getSharedPreferences("LANG_AUTH", MODE_PRIVATE);
+        String username = pref.getString("username", null);
+        String password = pref.getString("password", null);
+        if(username != null && password != null){
+            emailEd.setText(username);
+            passwordEd.setText(password);
+        }
+        loginBtn.setOnClickListener(this);
+        signupBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_login){
+            login();
+        }
+        else if(view.getId() == R.id.btn_signup){
+            if(mAuth.getCurrentUser() == null) {
+                Intent intent = new Intent(this, SignupActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void login(){
+        mAuth.signInWithEmailAndPassword(emailEd.getText().toString(), passwordEd.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getBaseContext(), "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/learn_english/Activity/SignupActivity.java
@@ -0,0 +1,81 @@
+package com.example.learn_english.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.learn_english.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.core.FirestoreClient;
+import com.google.firebase.firestore.ktx.FirestoreKt;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText emailEd, passwordEd, fullnameEd;
+    private Button signupBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        initUI();
+    }
+
+    private void initUI(){
+        emailEd = findViewById(R.id.ed_email);
+        passwordEd = findViewById(R.id.ed_password);
+        fullnameEd = findViewById(R.id.ed_fullname);
+        signupBtn = findViewById(R.id.btn_signup);
+        signupBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_signup){
+            signup();
+        }
+    }
+
+    private void signup() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null)
+            mAuth.signOut();
+
+        mAuth.createUserWithEmailAndPassword(emailEd.getText().toString(), passwordEd.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                            HashMap<String, String> myMap = new HashMap<>();
+                            myMap.put("fullname", fullnameEd.getText().toString());
+                            firestore.collection("account").document(user.getUid()).set(myMap);
+                            Toast.makeText(getBaseContext(), "Tạo tài khoản thành công!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(getBaseContext(), "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
